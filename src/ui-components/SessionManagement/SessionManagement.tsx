3e2e860a2e05a4411b13f35896b382ab93ba1401
duytung95nb <duytung95nb@gmail.com>
2024-04-28 10:33:02 +0700

    Enable post message to any origin (#7)

--- src/ui-components/SessionManagement/SessionManagement.tsx
+++ src/ui-components/SessionManagement/SessionManagement.tsx
@@ -1,52 +1,56 @@
-import React, { Fragment, useEffect } from 'react';
-import { Options, sessionCheckIframeId } from './SessionManagement.types';
+import React, { Fragment, useEffect } from "react";
+import { Options, sessionCheckIframeId } from "./SessionManagement.types";
 
-export const SessionManagement: React.FC<React.PropsWithChildren<{
-    options: Options,
-    sessionChanged: (e) => void
-}>> = ({
-    children,
-    options,
-    sessionChanged
-}) => {
-    const checkSessionIntervalInMs = !options.checkSessionInterval ? 5 * 1000
-        : options.checkSessionInterval;
-    useEffect(() => {
-        const checkSession = () => {
-            console.log('Checking session', sessionCheckIframeId);
-            const win = (document.getElementById(sessionCheckIframeId) as HTMLIFrameElement).contentWindow;
+export const SessionManagement: React.FC<
+  React.PropsWithChildren<{
+    options: Options;
+    sessionChanged: (e) => void;
+  }>
+> = ({ children, options, sessionChanged }) => {
+  const checkSessionIntervalInMs = !options.checkSessionInterval
+    ? 5 * 1000
+    : options.checkSessionInterval;
+  useEffect(() => {
+    const checkSession = () => {
+      console.log("Checking session", sessionCheckIframeId);
+      const win = (
+        document.getElementById(sessionCheckIframeId) as HTMLIFrameElement
+      ).contentWindow;
 
-            console.log('Checking session id', options.getSessionIdFn());
-            win!.postMessage(options.getSessionIdFn(), options.idpOrigin);
-        }
-        checkSession();
-        const timerId = setInterval(checkSession, checkSessionIntervalInMs);
-        const receiveMessage = (e) => {
-            console.log('Target origin', options.idpOrigin);
-            console.log('Event origin', e.origin);
-            if (e.origin !== options.idpOrigin) {
-                return;
-            }
-            if (e.data === 'changed') {
-                clearInterval(timerId);
-                // then take the actions below...
-                // Emit session changed event
-                sessionChanged(e);
-            }
-        }
-        window.addEventListener('message', receiveMessage);
-        return () => {
-            window.removeEventListener('message', receiveMessage);
-            clearInterval(timerId);
-        }
-    }, [sessionChanged]);
-    return (
-        <Fragment>
-            <iframe src={options.idpOrigin + options.checkSessionIframePath} id={sessionCheckIframeId}
-                style={{
-                    display: 'none'
-                }}></iframe>
-            {children}
-        </Fragment>
-    );
-}
+      console.log("Checking session id", options.getSessionIdFn());
+      win!.postMessage(options.getSessionIdFn(), "*");
+    };
+    checkSession();
+    const timerId = setInterval(checkSession, checkSessionIntervalInMs);
+    const receiveMessage = (e) => {
+      console.log("Target origin", options.idpOrigin);
+      console.log("Event origin", e.origin);
+      if (e.origin !== options.idpOrigin) {
+        return;
+      }
+      if (e.data === "changed") {
+        clearInterval(timerId);
+        // then take the actions below...
+        // Emit session changed event
+        sessionChanged(e);
+      }
+    };
+    window.addEventListener("message", receiveMessage);
+    return () => {
+      window.removeEventListener("message", receiveMessage);
+      clearInterval(timerId);
+    };
+  }, [sessionChanged]);
+  return (
+    <Fragment>
+      <iframe
+        src={options.idpOrigin + options.checkSessionIframePath}
+        id={sessionCheckIframeId}
+        style={{
+          display: "none",
+        }}
+      ></iframe>
+      {children}
+    </Fragment>
+  );
+};
